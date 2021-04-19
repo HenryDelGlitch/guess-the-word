c3b609af7ddb8f4858e0038b812b5603b79ffdfc
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-04-18 22:54:19 -0400

    Added functions to show guessed letters, update word in progress and check if the player won

--- js/script.js
+++ js/script.js
@@ -1,73 +1,98 @@
 const guessedLetters = document.querySelector(".guessed-letters");
-
 const guess = document.querySelector(".guess");
-
 const userGuess = document.querySelector(".letter");
-
 const wordIP = document.querySelector(".word-in-progress");
-
-const remainingGuessesElement = document.querySelector(".remaining");
-
+const remainingGuesses = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
-
 const message = document.querySelector(".message");
-
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetterArray = [];
 
-const guessedLettersArray = [];
-
-const hiddenWord = function(word) {
-
-    const placeholderLetters = [];
-    
-        for (const letter of word) {
-            placeholderLetters.push("●");
-        }
-
-    wordIP.innerText = placeholderLetters.join("");
-
+// Hide the words with circle symbols
+const hiddenWord = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    placeholderLetters.push("●");
+  }
+  wordIP.innerText = placeholderLetters.join("");
 };
 
 hiddenWord(word);
 
-guess.addEventListener("click", function(e){
-    e.preventDefault();
+guess.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Empty message 
+  message.innerText = "";
+  // Letter input from user
+  const guess = userGuess.value;
+  // Check if the input was one letter
+  const goodGuess = validateInput(guess);
 
-    message.innerText = "";
-    
-    const inputValue = userGuess.value;
-
-    const validateValue = validatePlayer(inputValue);
-
-    if(validateValue !== undefined){
-        makeGuess(validateValue);
-    }
-
-    userGuess.value = "";
+  if (goodGuess) {
+    // Check if it's part of the word
+    makeGuess(guess);
+  }
+  userGuess.value = "";
 });
 
-const validatePlayer = function(input) {
-    const acceptedLetter = /[a-zA-Z]/;
-
-    if(input.length === 0){
-        message.innerText = "You have to enter a letter!";
-    }else if(input.length > 1){
-        message.innerText = "You have to enter one letter!";
-    }else if(!input.match(acceptedLetter)){
-        message.innerText = "You have to enter a letter!";
-    }else{
-        return input;
-    }
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Please enter a letter.";
+  } else if (input.length > 1) {
+    message.innerText = "Please enter a single letter.";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter a letter from A to Z.";
+  } else {
+    return input;
+  }
 };
 
-const makeGuess = function(letter){
-    letter = letter.toUpperCase();
-    if(guessedLettersArray.includes(guessedLetters)){
-        message.innerText = "You already guessed that!";
-    }else{
-        guessedLettersArray.push(letter);
-        console.log(guessedLettersArray);
+const makeGuess = function (letter) {
+  letter = letter.toUpperCase();
+  if (guessedLetterArray.includes(letter)) {
+    message.innerText = "You already guessed that letter, silly. Try again.";
+  } else {
+    guessedLetterArray.push(letter);
+    console.log(guessedLetterArray);
+    showGuessedLetters();
+    updateWordsIP(guessedLetterArray);
+  }
+};
+
+const showGuessedLetters = function () {
+  guessedLetters.innerHTML = "";
+  for (const letter of guessedLetterArray) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLetters.append(li);
+  }
+};
+
+const updateWordsIP = function (guessedLetterArray) {
+  const wordUpperCase = word.toUpperCase();
+  const wordArray = wordUpperCase.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    //If the guess is right then reveal it!
+    if (guessedLetterArray.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    //If not then keep it censored
+    } else {
+      revealWord.push("●");
     }
+  }
+  //Update the words in progress to the revealed word
+  wordIP.innerText = revealWord.join("");
+  //Check if all the letters are revealed
+  checkIfWin();
+};
+
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordIP.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+  }
 };
