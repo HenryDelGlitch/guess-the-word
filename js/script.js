6e41f7beb9004ed57589d87a96762cd8160518e0
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-04-18 22:28:01 -0400

    Validated users guess with regex and array to guessed letters.

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@ const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
 
+const guessedLettersArray = [];
+
 const hiddenWord = function(word) {
 
     const placeholderLetters = [];
@@ -32,11 +34,40 @@ hiddenWord(word);
 
 guess.addEventListener("click", function(e){
     e.preventDefault();
+
+    message.innerText = "";
     
     const inputValue = userGuess.value;
 
-    console.log(inputValue);
+    const validateValue = validatePlayer(inputValue);
+
+    if(validateValue !== undefined){
+        makeGuess(validateValue);
+    }
 
     userGuess.value = "";
-
 });
+
+const validatePlayer = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    if(input.length === 0){
+        message.innerText = "You have to enter a letter!";
+    }else if(input.length > 1){
+        message.innerText = "You have to enter one letter!";
+    }else if(!input.match(acceptedLetter)){
+        message.innerText = "You have to enter a letter!";
+    }else{
+        return input;
+    }
+};
+
+const makeGuess = function(letter){
+    letter = letter.toUpperCase();
+    if(guessedLettersArray.includes(guessedLetters)){
+        message.innerText = "You already guessed that!";
+    }else{
+        guessedLettersArray.push(letter);
+        console.log(guessedLettersArray);
+    }
+};
